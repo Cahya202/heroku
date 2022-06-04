3ab7877165d5bd821d6473fefcc3db6c2d729b2e
KOMPTUK-19 <cahyahisamalbani@gmail.com>
2022-06-04 10:09:58 +0700

    cahya yoi

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.json({
     type: 'get',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'Cahya hisam Albani',
+    npm: '2042016'
   });
 })
 
@@ -14,8 +14,8 @@ app.post('/', function (req, res) {
   res.json({
     type: 'post',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'Cahya hisam Albani',
+    npm: '2042016'
   });
 })
 
